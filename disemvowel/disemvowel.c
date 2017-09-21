ceb965bc478a889d60290f03f92bdb62680fd04a
Xaitheng Yang <yang3792@reliant.morris.umn.edu>
2017-09-21 12:02:09 -0500

    first attempt at disemvowel no successful solution. Need to handle no vowel cases and MorrisMN case

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -2,6 +2,45 @@
 
 #include "disemvowel.h"
 
+bool islowercasevowel (char str) {
+        if ((str == 'a' || str == 'e' || str == 'i' || str == 'o' || str == 'u') == 1){
+                return true;
+                }
+        return false;
+}
+
+bool isuppercasevowel (char str) {
+        if ((str == 'A' || str == 'E' || str == 'I' || str == 'O' || str == 'U') == 1){
+                return true;
+                }
+        return false;
+}
+
 char* disemvowel(char* str) {
-  return (char*) "";
+  if (str[0] == '\0'){
+	  return NULL;
+  }
+  
+  int vowelcounter;
+  int i = 0;
+  while (str[i] != '\0')
+  {
+	if (isuppercasevowel(str[i]) || islowercasevowel(str[i]))
+	{
+		vowelcounter++;
+	}
+	i++;
+  }
+  char* disemvoweled;
+  disemvoweled = (char*) calloc (i - vowelcounter + 1, sizeof(char));
+  
+  i = 0;
+  while (str[i] != '\0') {
+	  if (!isuppercasevowel(str[i]) && !islowercasevowel(str[i])) {
+		disemvoweled += str[i];
+	  }
+	  i++;
+  }
+  disemvoweled += '\0';
+  return disemvoweled;
 }
